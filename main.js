cc4d1725aabffc761a8ef08d912f1ba7a1f7f917
Santhos <santhoshsunthar@gmail.com>
2023-02-10 18:58:32 +0530

    Changed in #mobile-menu-list scroll behaviour

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
+let body = document.body;
 const menuIcon = document.querySelector('.mobile-menu-icon');
 const menuList = document.querySelector('.mobile-menu-list');
 
 menuIcon.addEventListener('click', function () {
     menuList.style.display = menuList.style.display === 'block' ? 'none' : 'block';
+    body.style.overflow = body.style.overflow === "hidden" ? "scroll" : "hidden";
 });
