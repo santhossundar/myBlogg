19aa774a36391acdef8e1791b607f2e0bafbe410
Santhos <santhoshsunthar@gmail.com>
2023-02-11 12:32:00 +0530

    Fixed #mobile-menu-list visibility

--- main.js
+++ main.js
@@ -3,6 +3,6 @@ const menuIcon = document.querySelector('.mobile-menu-icon');
 const menuList = document.querySelector('.mobile-menu-list');
 
 menuIcon.addEventListener('click', function () {
-    menuList.style.display = menuList.style.display === 'block' ? 'none' : 'block';
+    menuList.style.visibility = menuList.style.visibility === 'visible' ? 'hidden' : 'visible';
     body.style.overflow = body.style.overflow === "hidden" ? "scroll" : "hidden";
 });
