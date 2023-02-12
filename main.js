c7ab7a306b8c02ac0c9097c72bcb911ba65f50ed
Santhos <santhoshsunthar@gmail.com>
2023-02-12 22:25:14 +0530

    Added #search-bar functionality

--- main.js
+++ main.js
@@ -1,8 +1,14 @@
 let body = document.body;
-const menuIcon = document.querySelector('.mobile-menu-icon');
-const menuList = document.querySelector('.mobile-menu-list');
+let menuIcon = document.querySelector('.mobile-menu-icon');
+let menuList = document.querySelector('.mobile-menu-list');
 
 menuIcon.addEventListener('click', function () {
     menuList.style.visibility = menuList.style.visibility === 'visible' ? 'hidden' : 'visible';
     body.style.overflow = body.style.overflow === "hidden" ? "scroll" : "hidden";
+});
+
+let searchIcon = document.querySelector('.search-icon');
+
+searchIcon.addEventListener('click', () => {
+    console.log("Searching ...");
 });
